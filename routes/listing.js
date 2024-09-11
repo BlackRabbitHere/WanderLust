b12e6b64f24c9f4984cd0f195695bcca827ac74d
Kshitij <kshitijkumar514@gmail.com>
2024-09-11 23:45:48 +0530

    Restyled the Review Part and Starting phase of upload files

--- routes/listing.js
+++ routes/listing.js
@@ -4,32 +4,33 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const Listing=require("../models/listing.js") // require the exported module
 const {validateListing,isOwner,isLoggedIn}=require("../middleware.js");
 const listingController=require("../controllers/listing.js")
+const multer=require('multer')
+const upload=multer({dest:'uploads/'})
 
 
-// Index Route get /Listings for showing all cards
-router.get("/",wrapAsync(listingController.index));
+
+router
+    .route("/")
+    // Index Route get /Listings for showing all cards
+    .get(wrapAsync(listingController.index))
+    //create Route
+    .post(isLoggedIn,validateListing,wrapAsync(listingController.createListing))
 
 // New & create Route 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
 
-//create Route
-
-router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing))
-
-// Show route /listing/:id
-router.get("/:id",wrapAsync(listingController.showListing));
+router
+    .route("/:id")
+    // Show route /listing/:id
+    .get(wrapAsync(listingController.showListing))
+    //update
+    .put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+    // Delete Route
+    .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
 
 // Edit 
 router.get("/:id/edit",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.renderEditForm));
-
-
-//Update
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
-
-// Delete Route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
-
 
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -7,18 +7,21 @@ const {saveRedirectUrl}=require("../middleware")
 const userController=require("../controllers/user")
 
 
-router.get("/signup",userController.renderSignupForm);
+router
+    .route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup))
 
-router.post("/signup",wrapAsync(userController.signup))
+router
+    .route("/login")
+    .get(userController.renderLoginForm)
+    .post(saveRedirectUrl,
+        passport.authenticate("local",
+            {failureRedirect:'/user/login', 
+            failureFlash:true})
+            ,userController.login
+    )
 
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login",saveRedirectUrl,
-    passport.authenticate("local",
-        {failureRedirect:'/user/login', 
-        failureFlash:true})
-        ,userController.login
-)
 
 router.get("/logout",userController.logout)
 
