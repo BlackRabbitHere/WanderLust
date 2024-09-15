07c4097cd1ee6d0babbfc0a161fa6942325699e3
Kshitij <kshitijkumar514@gmail.com>
2024-09-15 10:57:13 +0530

    Markers added to maps in each Listing

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing")
+const mbxGeocoding=require('@mapbox/mapbox-sdk/services/geocoding')
+const mapToken=process.env.MAP_TOKEN;
+const geocodingClient= mbxGeocoding({accessToken:mapToken})
 
 
 module.exports.index = async (req,res)=>{
@@ -25,6 +28,11 @@ module.exports.showListing = async(req,res,next)=>{
 
 
 module.exports.createListing = async(req,res,next)=>{
+    let response=await geocodingClient.forwardGeocode({
+        query:req.body.listing.location,
+        limit:1
+    })
+    .send()
     let url=req.file.path;
     let filename=req.file.filename;
     console.log(url,"..",filename);
@@ -37,8 +45,10 @@ module.exports.createListing = async(req,res,next)=>{
     // console.log(req.user);
     newListing.owner=req.user._id;
     newListing.image={url,filename};
-    await newListing.save();
+    newListing.geometry=response.body.features[0].geometry;
+    let savedListing=await newListing.save();
     req.flash("success","New Listing Created!");
+    console.log(savedListing);
     console.log(req.flash)
     res.redirect("/listings");
     console.log(listing);
