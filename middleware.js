a194a6173b6d9d404fb13cf46846fd4e0134e4d9
BlackRabbitHere <163437319+BlackRabbitHere@users.noreply.github.com>
2024-09-22 00:37:05 +0530

    Correct the flash part for reviews

--- middleware.js
+++ middleware.js
@@ -41,7 +41,7 @@ module.exports.isReviewAuthor = async (req,res,next)=>{
     let { id,reviewId}= req.params;
     let review = await Review.findById(reviewId);
     if(!review.author.equals(res.locals.currUser._id)){
-        req.flash("error","You are no the author of this review");
+        req.flash("error","You are not the author of this review");
         return res.redirect(`/listings/${id}`)
     }
     next();
